590825fd4d02ad5f2168cf053356d29759406ea4
Yasaman Sefidgar <einsian@cs.washington.edu>
2020-12-08 11:50:17 -0800

    test code with the secure server (GET request)

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient , HttpHeaders} from '@angular/common/http';
 import { NavController } from 'ionic-angular';
 import { SignUpPage } from "../signup/signup";
 import { HomePage } from "../home/home";
 import { CouchDbServiceProvider } from "../../providers/couch-db-service/couch-db-service";
 import { GlobalFunctionsServiceProvider } from "../../providers/global-functions-service/global-functions-service";
 import { Storage } from '@ionic/storage';
+import { Injectable } from '@angular/core'; //YSS
 
+@Injectable() //YSS
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
@@ -31,6 +33,34 @@ export class LoginPage {
       username: this.username,
       password: this.password
     };
+    
+    console.log('YSS tests new db with GET')
+    //let get_url = 'https://b9c6a1ec-b33d-467d-99b7-63e02c7bb018.mock.pstmn.io/test' // the mock server does not have any headers in its response and the get request is successful
+    let get_url = 'https://tractdb.org/';  
+    let get_headers1 = new HttpHeaders()
+    get_headers1 = get_headers1.set('Accept', '*/*');
+    get_headers1 = get_headers1.set('Content-Type','text/plain')
+    console.log('YSS GET headers 1', JSON.stringify(get_headers1))
+    let get_headers2 = new HttpHeaders({
+     'Accept': '*/*',
+     'Content-Type': 'text/plain'
+    })
+    console.log('YSS GET headers 2', JSON.stringify(get_headers2))
+    let get_headers3 = new HttpHeaders()
+    get_headers3 = get_headers3.append('Accept', '*/*');
+    get_headers3 = get_headers3.append('Content-Type', 'text/plain');
+    console.log('YSS GET headers 3', JSON.stringify(get_headers3))
+    this.http.get(get_url, {observe: 'body', responseType: 'text'})
+    .subscribe({
+      next: data => {
+          console.log('YSS GET success', JSON.stringify(data))
+      },
+      error: error => {
+          console.error('YSS GET error', JSON.stringify(error))
+      }
+    })
+    // {observe: 'body', responseType: 'text', headers: get_headers1} did not work
+
     this.couchDbService.login(credentials).then(response => {
       console.log("Log in as " + credentials['username']);
       this.storage.set('credentials', credentials);
