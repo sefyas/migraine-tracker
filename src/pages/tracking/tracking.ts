de95399beb6419af8d4f23a139c2d0fe41e151d8
Yasaman Sefidgar <einsian@cs.washington.edu>
2021-01-28 13:51:52 -0800

    removed the per page reset

--- src/pages/tracking/tracking.ts
+++ src/pages/tracking/tracking.ts
@@ -83,7 +83,6 @@ export class TrackingPage {
     //console.log("eraser icon clicked for goal", goal, " and data", data);
     //console.log("value for this goal-data is", this.tracked[goal][data['id']], "among tracked data", this.tracked);
     delete this.tracked[goal][data['id']];
-    //console.log("which is erased and tracked data is now", this.tracked);
     // YSS TO-DO investigate whether delete this.tracked[goal] is also applicable when all data under a certain goal is erased
   }
   // ================= BUTTONS Ends =================
